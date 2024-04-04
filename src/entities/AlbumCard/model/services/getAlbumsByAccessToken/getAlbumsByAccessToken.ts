84e8c4fdef40d7f423ece0fda5fc018dc711703b
def1s <temnay2@icloud.com>
2024-04-04 21:28:36 +0300

    add logic for update access token

--- src/entities/AlbumCard/model/services/getAlbumsByAccessToken/getAlbumsByAccessToken.ts
+++ src/entities/AlbumCard/model/services/getAlbumsByAccessToken/getAlbumsByAccessToken.ts
@@ -3,31 +3,46 @@ import { Album } from '../../types/userAlbumsSchema';
 import axios, { AxiosRequestConfig } from 'axios';
 import { ApiResponse } from 'shared/api/types/apiResponse';
 import { ACCESS_TOKEN_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
+import { getUpdatedAccessToken } from 'shared/api/services/getUpdatedAccessToken/getUpdatedAccessToken';
+// ПОКА ЧТО ВРЕМЕННО НАРУШАЮ FSD
+import { userActions } from 'entities/User';
 
 export const getAlbumsByAccessToken = createAsyncThunk<Album[], null, { rejectValue: string }>(
 	'albums/getAlbumsByAccessToken',
 	async (_, thunkAPI) => {
-		try {
-			const accessToken = localStorage.getItem(ACCESS_TOKEN_LOCALSTORAGE_KEY);
+		const accessToken = localStorage.getItem(ACCESS_TOKEN_LOCALSTORAGE_KEY);
 
-			const options: AxiosRequestConfig = {
-				method: 'GET',
-				url: 'http://localhost:8081/albums/getByUserId',
-				headers: {
-					'Authorization': `Bearer ${accessToken}`
-				}
-			};
-
-			const response = await axios<ApiResponse<Album>>(options);
-
-			if (!response.data) {
-				console.log('empty');
+		const options: AxiosRequestConfig = {
+			method: 'GET',
+			url: 'http://localhost:8081/albums/getByUserId',
+			headers: {
+				'Authorization': `Bearer ${accessToken}`
 			}
+		};
 
+		try {
+			const response = await axios<ApiResponse<Album>>(options);
 			return response.data.data;
 		} catch (error) {
-			console.log(error);
-			return thunkAPI.rejectWithValue('Произошла ошибка!');
+			if (error.response && error.response.status === 403) {
+				try {
+					const updatedToken = await getUpdatedAccessToken();
+					if (updatedToken) {
+						localStorage.setItem(ACCESS_TOKEN_LOCALSTORAGE_KEY, updatedToken);
+						options.headers['Authorization'] = `Bearer ${updatedToken}`;
+						const response = await axios<ApiResponse<Album>>(options);
+						return response.data.data;
+					}
+				} catch (error) {
+					if (error.response && error.response.status === 403) {
+						// ПОКА ЧТО ВРЕМЕННО НАРУШАЮ FSD
+						thunkAPI.dispatch(userActions.logout());
+						return thunkAPI.rejectWithValue('Необходима повторная авторизация');
+					}
+				}
+			}
+
+			return thunkAPI.rejectWithValue(error.response.data.message || 'Непредвиденная ошибка');
 		}
 	}
 );
